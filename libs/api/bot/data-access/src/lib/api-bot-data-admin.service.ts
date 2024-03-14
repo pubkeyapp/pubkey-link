e2ede8568528dc04313757640df06e82e1082003
Bram Borggreve <beeman@beeman.nl>
2024-03-14 04:02:17 +0000

    refactor: clean up api bot feature

---
+++ libs/api/bot/data-access/src/lib/api-bot-data-admin.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common'
+import { ApiBotDataService } from './api-bot-data.service'
+import { AdminCreateBotInput } from './dto/admin-create-bot.input'
+import { AdminFindManyBotInput } from './dto/admin-find-many-bot.input'
+import { AdminUpdateBotInput } from './dto/admin-update-bot.input'
+import { BotPaging } from './entity/bot.entity'
+import { getAdminBotWhereInput } from './helpers/get-admin-bot-where.input'
+
+@Injectable()
+export class ApiBotDataAdminService {
+  private readonly logger = new Logger(ApiBotDataAdminService.name)
+  constructor(private readonly data: ApiBotDataService) {}
+
+  async createBot(input: AdminCreateBotInput) {
+    return this.data.create(input)
+  }
+
+  async deleteBot(botId: string) {
+    return this.data.delete(botId)
+  }
+
+  async findManyBot(input: AdminFindManyBotInput): Promise<BotPaging> {
+    return this.data.findMany({
+      orderBy: { createdAt: 'desc' },
+      where: getAdminBotWhereInput(input),
+      limit: input.limit,
+      page: input.page,
+    })
+  }
+
+  async findOneBot(botId: string) {
+    return this.data.findOne(botId)
+  }
+
+  async updateBot(botId: string, input: AdminUpdateBotInput) {
+    return this.data.update(botId, input)
+  }
+}
