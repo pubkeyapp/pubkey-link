8dc7a115cb1e3f90ebe598a37f1f072f33105210
Bram Borggreve <beeman@beeman.nl>
2024-03-14 01:41:02 +0000

    refactor: clean up api network-token feature

--- libs/api/network-token/data-access/src/lib/api-network-token.data-access.module.ts
+++ libs/api/network-token/data-access/src/lib/api-network-token.data-access.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common'
 import { ApiCoreDataAccessModule } from '@pubkey-link/api-core-data-access'
 import { ApiNetworkDataAccessModule } from '@pubkey-link/api-network-data-access'
+import { ApiNetworkTokenDataAdminService } from './api-network-token-data-admin.service'
+import { ApiNetworkTokenDataUserService } from './api-network-token-data-user.service'
+import { ApiNetworkTokenDataService } from './api-network-token-data.service'
 import { ApiNetworkTokenService } from './api-network-token.service'
-import { ApiAdminNetworkTokenService } from './api-admin-network-token.service'
-import { ApiUserNetworkTokenService } from './api-user-network-token.service'
 
 @Module({
   imports: [ApiCoreDataAccessModule, ApiNetworkDataAccessModule],
-  providers: [ApiNetworkTokenService, ApiAdminNetworkTokenService, ApiUserNetworkTokenService],
+  providers: [
+    ApiNetworkTokenService,
+    ApiNetworkTokenDataService,
+    ApiNetworkTokenDataAdminService,
+    ApiNetworkTokenDataUserService,
+  ],
   exports: [ApiNetworkTokenService],
 })
 export class ApiNetworkTokenDataAccessModule {}
--- libs/api/network-token/data-access/src/lib/helpers/get-network-token-where-admin.input.ts
+++ libs/api/network-token/data-access/src/lib/helpers/get-network-token-where-admin.input.ts
@@ -1,7 +1,7 @@
 import { Prisma } from '@prisma/client'
 import { AdminFindManyNetworkTokenInput } from '../dto/admin-find-many-network-token.input'
 
-export function getAdminNetworkTokenWhereInput(input: AdminFindManyNetworkTokenInput): Prisma.NetworkTokenWhereInput {
+export function getNetworkTokenWhereAdminInput(input: AdminFindManyNetworkTokenInput): Prisma.NetworkTokenWhereInput {
   const where: Prisma.NetworkTokenWhereInput = {
     cluster: input.cluster,
   }
