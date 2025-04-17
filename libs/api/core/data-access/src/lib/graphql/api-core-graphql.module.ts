4e1a0dfc6ffdb8d7ca2679d87b20fc2d67b44e0b
Ian Lovett <ilovett@opuslogica.com>
2025-04-17 13:24:58 -0700

    fix: allow disabling graphql playground

--- libs/api/core/data-access/src/lib/graphql/api-core-graphql.module.ts
+++ libs/api/core/data-access/src/lib/graphql/api-core-graphql.module.ts
@@ -4,18 +4,20 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { AppContext } from '../entity/app-context'
 import { join } from 'path'
 
+const isPlaygroundEnabled = process.env['GRAPHQL_PLAYGROUND']?.toLowerCase() === 'true';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: join(process.cwd(), 'api-schema.graphql'),
       sortSchema: true,
       driver: ApolloDriver,
-      introspection: process.env['GRAPHQL_PLAYGROUND']?.toLowerCase() === 'true',
-      playground: {
+      introspection: isPlaygroundEnabled,
+      playground: isPlaygroundEnabled ? {
         settings: {
           'request.credentials': 'include',
         },
-      },
+      } : false,
       resolvers: {
         // JSON: GraphQLJSON,
       },
